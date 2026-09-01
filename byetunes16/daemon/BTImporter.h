5f9196ffc33efc07cf735ca855c32dd8f15b9e40
Zyn <bobbytatum999@gmail.com>
2026-08-31 20:50:58 -0500

    Expand ByeTunes16 daemon feature API

--- byetunes16/daemon/BTImporter.h
+++ byetunes16/daemon/BTImporter.h
@@ -4,8 +4,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface BTImporter : NSObject
 + (NSDictionary *)probe;
-+ (NSDictionary *)importFileAtPath:(NSString *)path sourceName:(NSString * _Nullable)sourceName;
++ (NSDictionary *)importFileAtPath:(NSString *)path
+                        sourceName:(NSString * _Nullable)sourceName
+                          metadata:(NSDictionary * _Nullable)metadata
+                      playlistName:(NSString * _Nullable)playlistName;
++ (NSDictionary *)libraryWithLimit:(NSUInteger)limit;
++ (NSDictionary *)playlists;
++ (NSDictionary *)createPlaylistNamed:(NSString *)name itemPIDs:(NSArray<NSNumber *> *)itemPIDs;
++ (NSDictionary *)updateMetadataForItemPID:(int64_t)itemPID metadata:(NSDictionary *)metadata;
++ (NSDictionary *)deleteItemPID:(int64_t)itemPID;
++ (NSDictionary *)createBackup;
 + (NSDictionary *)restoreLatestBackup;
++ (NSDictionary *)repairLibrary;
 @end
 
 NS_ASSUME_NONNULL_END
