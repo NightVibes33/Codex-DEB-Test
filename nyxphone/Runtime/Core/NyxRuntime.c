4add0c76f61144ad6c1e4fbc546bdcda26dc6f26
Zyn <bobbytatum999@gmail.com>
2026-09-05 07:44:04 -0500

    Execute dyld bit manipulation instructions

--- nyxphone/Runtime/Core/NyxRuntime.c
+++ nyxphone/Runtime/Core/NyxRuntime.c
@@ -50,7 +50,7 @@ static void nyx_emit(NyxVM *vm, const char *message) {
 }
 
 const char *nyx_runtime_version(void) {
-    return "NyxRuntime/0.18-ksurface-mediation";
+    return "NyxRuntime/0.19-dyld-bit-operations";
 }
 
 uint32_t nyx_runtime_abi_version(void) {
